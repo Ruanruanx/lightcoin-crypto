3f75ad43dc053da6c680819d067c49a71b39ab1b
Ruanruanx <zxin7@uwo.ca>
2022-08-01 19:31:06 +0000

    need to fix balance

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account{
+  constructor(username){
+    this.username = username;
+    this.transactions = [];
   }
+  get balance(){
+    let balance = 0;
+    balance = this.transactions.reduce((a,b)=>
+      a+b,0 )
+    return balance;
+  }
+  addTransaction(transaction){
+    this.transactions.push(transaction);
+  }
+}
 
-  commit() {
-    balance -= this.amount;
+class Transaction{
+  constructor(amount,account){
+    this.amount = amount;
+    this.account = account;
+  }
+  commit(){
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
+}
+
+class Withdrawal extends Transaction {
+  // commit() {
+  //   this.account.balance -= this.amount;
+  // }
+  get value(){
+    return  -this.amount;
   }
 
 }
 
+class Deposit extends Transaction {
+  // commit(){
+  //   this.account.balance += this.amount;
+  // }
+  get value(){
+    return  this.amount;
+  }
+}
 
+const myAccount = new Account("snow-patrol")
+t1 = new Withdrawal(50.25,myAccount);
+t1.commit();//commit the change
+console.log("starting balance", t1.balance)
 
-
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+t2 = new Deposit(1000, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log(t2.balance)
+// t1 = new Withdrawal(50.25,myAccount);
+// t1.commit();
+// console.log('Transaction 1: ', t1)
 
-console.log('Balance:', balance);
+// t2 = new Withdrawal(9.99,myAccount);
+// t2.commit();
+// console.log('Transaction 2: ', t2);
+// console.log('balance' ,balance);
+
+// t3 = new Deposit(1000,myAccount);
+// t3.commit();
+// console.log('Transaction 3: ', t3);
+// console.log('balance', balance)
